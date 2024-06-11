15540ba334b60b5bde25e5326578e09a7bd30f5d
pizzapatrik <pizzapatrik@proton.me>
2024-06-11 15:12:05 +0200

    small fixes

--- Java/TestVehicleReportApp.java
+++ Java/TestVehicleReportApp.java
@@ -41,6 +41,7 @@ public class TestVehicleReportApp {
 			System.out.println("Fee "+chargedTollEvent.getTollFee().getAmountToPay());
 			System.out.println("Event type: "+chargedTollEvent.getType().name());
 			System.out.println("Event description:"+chargedTollEvent.getType().getDescription());
+			System.out.println("Interval start:"+chargedTollEvent.getIntervalStart());
 			System.out.println("-----------------------------------");
 		}
 
@@ -51,6 +52,7 @@ public class TestVehicleReportApp {
 			System.out.println("Time "+noneChargedTollEvent.getDate());
 			System.out.println("Event type: "+noneChargedTollEvent.getType().name());
 			System.out.println("Event description: "+noneChargedTollEvent.getType().getDescription());
+			System.out.println("Interval start:"+noneChargedTollEvent.getIntervalStart());
 			System.out.println("-----------------------------------");
 
 		}
@@ -70,43 +72,52 @@ public class TestVehicleReportApp {
 //		Fee 18
 //		Event type: CHARGEABLE_EVENT_TYPE
 //		Event description:The vehicle for this event is not toll free and the event occurred during a chargeable hour.
+//		Interval start:2024-06-03T06:15
 //		-----------------------------------
 //		Time 07:20
 //		Fee 18
 //		Event type: CHARGEABLE_EVENT_TYPE
 //		Event description:The vehicle for this event is not toll free and the event occurred during a chargeable hour.
+//		Interval start:2024-06-03T07:20
 //		-----------------------------------
 //		Time 12:15
 //		Fee 8
 //		Event type: CHARGEABLE_EVENT_TYPE
 //		Event description:The vehicle for this event is not toll free and the event occurred during a chargeable hour.
+//		Interval start:2024-06-03T12:15
 //		-----------------------------------
 //		Time 13:20
 //		Fee 8
 //		Event type: CHARGEABLE_EVENT_TYPE
 //		Event description:The vehicle for this event is not toll free and the event occurred during a chargeable hour.
+//		Interval start:2024-06-03T13:20
 //		-----------------------------------
 //		Time 15:45
 //		Fee 8
 //		Event type: PARTIAL_DEDUCTED_ON_MAX_DAILY_EVENT_TYPE
 //		Event description:The vehicle for this event is not toll free and the event occurred during chargeable hours but maximum daily fee is reached when adding this event
+//		Interval start:2024-06-03T15:20
 //		-----------------------------------
 //		---- None charged Toll Events ----
 //		Time 05:15
 //		Event type: TOLL_FREE_DAY_TIME_EVENT_TYPE
 //		Event description: This event occurred during none chargeable hours.
+//		Interval start:2024-06-03T05:15
 //		-----------------------------------
 //		Time 06:15
 //		Event type: DEDUCTED_ON_HOURLY_INTERVAL_EVENT_TYPE
 //		Event description: The vehicle for this event is not toll free and the event occurred during chargeable hours but the amount is deducted due to another event whithin the same span of one hour with same or higher fee.
+//		Interval start:2024-06-03T06:15
 //		-----------------------------------
 //		Time 12:30
 //		Event type: DEDUCTED_ON_HOURLY_INTERVAL_EVENT_TYPE
 //		Event description: The vehicle for this event is not toll free and the event occurred during chargeable hours but the amount is deducted due to another event whithin the same span of one hour with same or higher fee.
+//		Interval start:2024-06-03T12:15
 //		-----------------------------------
 //		Time 15:20
 //		Event type: DEDUCTED_ON_HOURLY_INTERVAL_EVENT_TYPE
 //		Event description: The vehicle for this event is not toll free and the event occurred during chargeable hours but the amount is deducted due to another event whithin the same span of one hour with same or higher fee.
+//		Interval start:2024-06-03T15:20
 //		-----------------------------------
 
 		
--- Java/model/toll/event/TollEvenType.java
+++ Java/model/toll/event/TollEvenType.java
@@ -6,8 +6,8 @@ package model.toll.event;
 public enum TollEvenType {
 	
 	   CHARGEABLE_EVENT_TYPE("The vehicle for this event is not toll free and the event occurred during a chargeable hour.", false ),
-	   DEDUCTED_ON_HOURLY_INTERVAL_EVENT_TYPE("The vehicle for this event is not toll free and the event occurred during chargeable hours but the amount is deducted due to another event whithin the same span of one hour with same or higher fee.", false),
-	   PARTIAL_DEDUCTED_ON_MAX_DAILY_EVENT_TYPE("The vehicle for this event is not toll free and the event occurred during chargeable hours but maximum daily fee is reached when adding this event", false),
+	   DEDUCTED_ON_HOURLY_INTERVAL_EVENT_TYPE("The vehicle for this event is not toll free and the event occurred during chargeable hours but the amount is deducted due to another event whithin with a higher fee in the same interval.", false),
+	   PARTIAL_DEDUCTED_ON_MAX_DAILY_EVENT_TYPE("The vehicle for this event is not toll free and the event occurred during chargeable hours but maximum daily amount was reached and the fee was partialy deducted", false),
 	   FULLY_DEDUCTED_ON_MAX_DAILY_EVENT_TYPE("The vehicle for this event is not toll free and the event occurred during chargeable hours but maximum daily fee is already reached", true),
 	   TOLL_FREE_DAY_TIME_EVENT_TYPE("This event occurred during none chargeable hours.", true),
 	   TOLL_FREE_VEHICLE_EVENT_TYPE("The vehicle in this event is toll free.", true),
